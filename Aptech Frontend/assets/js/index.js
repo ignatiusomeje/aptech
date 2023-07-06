3785c8e1463699492f61124da9de2f1dca64f651
ignatiusomeje <mrexcel153@gmail.com>
2023-07-06 20:00:01 +0100

    add sweet alert css and js. add and image to the html and create an image folder. add sweet alert js config and add try and catch to curtail errors in the index.js file. style the image in the scss file.

--- Aptech Frontend/assets/js/index.js
+++ Aptech Frontend/assets/js/index.js
@@ -1,32 +1,60 @@
-const form = document.getElementById('userData');
-const embed = document.getElementById('check');
-const fullName = document.querySelector('[name=fullName]')
-const email = document.querySelector('[name=email]')
-const whichSocialMedia = document.querySelectorAll('[name=whichSocialMedia]')
-console.log(fullName)
+const form = document.getElementById("userData");
+const embed = document.getElementById("check");
+const fullName = document.querySelector("[name=fullName]");
+const email = document.querySelector("[name=email]");
+const whichSocialMedia = document.querySelectorAll("[name=whichSocialMedia]");
 
-form.addEventListener('submit', async e =>{
-  e.preventDefault()
-  const formCheck = new FormData(e.target)
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const formCheck = new FormData(e.target);
   const userData = {
-    fullName: formCheck.get('fullName').trim() !== '' && formCheck.get('fullName'),
-    email: formCheck.get('email').trim() !== '' && formCheck.get('email'),
-    whichSocialMedia: formCheck.get('whichSocialMedia') !== '' && formCheck.get('whichSocialMedia')
+    fullName:
+      formCheck.get("fullName").trim() !== "" && formCheck.get("fullName"),
+    email: formCheck.get("email").trim() !== "" && formCheck.get("email"),
+    whichSocialMedia:
+      formCheck.get("whichSocialMedia") !== "" &&
+      formCheck.get("whichSocialMedia"),
+  };
+  if (
+    (userData.fullName || userData.email || userData.whichSocialMedia) !== null
+  ) {
+    try {
+      const file = await axios.post(
+        "http://127.0.0.1:3000/Api/v1/user/save",
+        userData,
+        {
+          responseType: "blob",
+        }
+      );
+      const url = URL.createObjectURL(file.data);
+      window.open(url, "__blank");
+      URL.revokeObjectURL(url);
+      fullName.value = "";
+      email.value = "";
+      whichSocialMedia.forEach((Element) => (Element.checked = false));
+    } catch (err) {
+      console.log(err);
+      if (err.response.status === 400) {
+        const Toast = Swal.mixin({
+          toast: true,
+          position: "top-end",
+          showConfirmButton: false,
+          timer: 3000,
+          timerProgressBar: true,
+          didOpen: (toast) => {
+            toast.addEventListener("mouseenter", Swal.stopTimer);
+            toast.addEventListener("mouseleave", Swal.resumeTimer);
+          },
+        });
+
+        Toast.fire({
+          icon: "error",
+          title: "Email already exists",
+        });
+        fullName.value = "";
+        email.value = "";
+        whichSocialMedia.forEach((Element) => (Element.checked = false));
+      }
+    }
   }
-  if ((userData.fullName || userData.email || userData.whichSocialMedia) !== null ){
-    const file = await axios.post('http://127.0.0.1:3000/Api/v1/user/save', userData,{
-      responseType: 'blob',
-    })
-    const url = URL.createObjectURL(file.data)
-    window.open(url, '__blank')
-    // const a = document.createElement('a');
-    // a.href = url;
-    // a.download = 'Free-Ebook 2023';
-    // a.click()
-    // a.remove()
-    URL.revokeObjectURL(url)
-    fullName.value = "";
-    email.value = ""
-    whichSocialMedia.forEach(Element=> Element.checked = false)
-  }
-})
+});
